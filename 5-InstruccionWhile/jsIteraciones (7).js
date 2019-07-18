16fda89e28f40cfc1b6917fb28c1f4159f9c23cf
florenciasanchez99 <47385930+florenciasanchez99@users.noreply.github.com>
2019-07-18 12:27:39 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -8,30 +8,52 @@ function mostrar()
 
   
 
-	do //HACER LO SIGUIENTE:
-{
+	//do //HACER LO SIGUIENTE:
+//{
 
 	//TOMO LA VARIABLE NUMERO, LA TRANFORMO A PARSEINT Y LE ASIGNO UNA CAJA DE TEXTO CON PROMPT.
-	numero =parseInt(prompt("Ingrese un numero"));
+	//numero =parseInt(prompt("Ingrese un numero"));
 
 
-  //SE AGREGA UN ACUMULADOR PARA HACER LAS SUMAS DE LOS NUMEROS QUE HAYA INGRESADO EL USUARIO.
-  acumulador = acumulador + numero;
+//SE AGREGA UN ACUMULADOR PARA HACER LAS SUMAS DE LOS NUMEROS QUE HAYA INGRESADO EL USUARIO.
+//acumulador = acumulador + numero;
 
 
- //PONEMOS LA VARIABLE RESPUESTA CON PROMPT PARA QUE APAREZCA CON LA CAJA DE TEXTO Y 
- //PARA PREGUNTAR AL USUARIO SI QUIERE SEGUIR AGREGANDO NUMEROS.
-  respuesta=prompt("¿Quiere ingresar mas numeros? "); 
+//PONEMOS LA VARIABLE RESPUESTA CON PROMPT PARA QUE APAREZCA CON LA CAJA DE TEXTO Y 
+//PARA PREGUNTAR AL USUARIO SI QUIERE SEGUIR AGREGANDO NUMEROS.
+//respuesta=prompt("¿Quiere ingresar mas numeros? "); 
 
 
- contador= contador +1;
+ //contador= contador +1;
+
+//}
+
+//while (respuesta=="si") //MIENTRAS ESTA CONDICION SE CUMPLA VA A SEGUIR APARECIENDO LA CAJA DE TEXTO PREGUNTANDO SI QUIERE AGREGAR NUMEROS.
+
+
+//document.getElementById("suma").value=acumulador;
+//document.getElementById("promedio").value=acumulador/contador;
+
+
+
+
+while(respuesta=="si")
+
+{
+
+numero=parseInt(prompt("Ingrese un nuemro"));
+
+acumulador = acumulador + numero;
+
+respuesta=prompt("¿Quiere ingresar mas numeros?");
+
+
+contador=contador +1;
 
 }
 
-while (respuesta=="si") //MIENTRAS ESTA CONDICION SE CUMPLA VA A SEGUIR APARECIENDO LA CAJA DE TEXTO PREGUNTANDO SI QUIERE AGREGAR NUMEROS.
-
-
 document.getElementById("suma").value=acumulador;
+
 document.getElementById("promedio").value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -2,24 +2,50 @@ function mostrar()
 {
 
 	var contador=0;
+	var acumulador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta="si";
 
-	do
-	{
-   
-
-  positivo=parseInt(prompt)
-
-	}
-   
 	while(respuesta=="si")
+
+	{
+         
+     numero=parseInt(prompt("Ingrese un numero"));
+	 
+	 respuesta=prompt("¿Quiere ingresar otro numero?");
+
+	 if(numero > 0 ) //PONGO NUMERO MAYOR A 0 PORQUE QUIERO Q LOS POSITIVOS SE SUMEN Y LOS NEGATIVOS SE MULTIPLIQU
+	 {
+
+		positivo=positivo + numero
+
+	 contador=contador + 1;
+
+       acumulador= acumulador + 1;
+
+	 }
+	  
+	else
+		{
+	  
+	
+
+		negativo=negativo * numero;
+
+
+	}	   
+	 
+
+
+	
+   
+	
 
 document.getElementById("suma").value=positivo;
 document.getElementById("producto").value=negativo;
 
 }//FIN DE LA FUNCIÓN
 
-
+}
